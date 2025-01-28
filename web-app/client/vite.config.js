a189c2d941e3c73c91ae789aeb6be4692848e9bd
Cory Versfeld <versfeldc@cardiff.ac.uk>
2025-01-28 21:29:44 +0000

    added force https

--- web-app/client/vite.config.js
+++ web-app/client/vite.config.js
@@ -2,5 +2,8 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+    server: {
+        https: true, // Force HTTPS in development
+    },
     plugins: [react()], // Add React plugin to handle JSX
 });
