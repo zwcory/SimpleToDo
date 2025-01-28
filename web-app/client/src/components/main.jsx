48273c265bc6486dcf8e6f4a9c47ed6568b311bd
Cory Versfeld <versfeldc@cardiff.ac.uk>
2025-01-28 22:57:18 +0000

    added analytics

--- web-app/client/src/components/main.jsx
+++ web-app/client/src/components/main.jsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import '../css/index.css'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { Analytics } from "@vercel/analytics/react";
+import '../css/index.css';
 import ToDo from "./ToDo.jsx";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <ToDo/>
+      <Analytics/>
   </StrictMode>,
 )
