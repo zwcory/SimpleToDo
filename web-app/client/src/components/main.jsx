cbd4a834005861e98b3508a1591bdee850f0e432
Cory Versfeld <versfeldc@cardiff.ac.uk>
2025-01-29 19:12:38 +0000

    added analytics, speed insights and seo

--- web-app/client/src/components/main.jsx
+++ web-app/client/src/components/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 import '../css/index.css';
 import ToDo from "./ToDo.jsx";
 
@@ -8,5 +9,6 @@ createRoot(document.getElementById('root')).render(
   <StrictMode>
     <ToDo/>
       <Analytics/>
+      <SpeedInsights/>
   </StrictMode>,
 )
